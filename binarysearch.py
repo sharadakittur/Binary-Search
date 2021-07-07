8e0fbeea797afd4be3beaecf9ff71140d94dc8fb
sharadakittur <86993394+sharadakittur@users.noreply.github.com>
2021-07-07 15:12:48 -0700

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+def binary(lst, itm):
+	nlst = []
+	for x in range(0, len(lst) - 1, -1):
+		nlst.append(x)
+	exp = 1
+	half = round(len(lst)/(2**exp))
+	for y in range(half):
+		if lst[y] == itm:
+			return y
+	del lst[0:len(lst)/2]
+	for z in range(half):
+		if nlst[z] == itm:
+			return z
+	del nlst[0:len(lst)/2]
+	exp += 1
+	
+print(binary(lst, 3))
